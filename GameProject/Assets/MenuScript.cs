2afc938ce35a1ee824c670f7a7fd71714759ee0d
Aldossari <sfaldossari1s@semo.edu>
2017-12-08 09:06:21 -0600

    Sarah's folder

---
+++ GameProject/Assets/MenuScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuScript : MonoBehaviour {
+
+    public Canvas QuitMenu;
+    public Button startText;// play button
+    public Button exitText;//exit button
+	// Use this for initialization
+	void Start () {
+
+        QuitMenu = QuitMenu.GetComponent<Canvas>();
+        startText = startText.GetComponent<Button>();
+        exitText = exitText.GetComponent<Button>();
+        QuitMenu.enabled = false;
+    }
+
+    public void ExitPress() {
+        QuitMenu.enabled = true;
+        startText.enabled = false;
+        exitText.enabled = false;
+    }
+
+    public void NoPress()
+    {
+        QuitMenu.enabled = false;
+        startText.enabled = true;
+        exitText.enabled = true;
+    }
+
+    public void StartLevel()
+    {
+        Application.LoadLevel(1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
+    
