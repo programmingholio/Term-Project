1ad076920a9ac92ee581dfab49ab9b086fa7d7f7
programmingholio <programingholio@gmail.com>
2017-12-09 18:33:30 -0600

    Map finished

---
+++ Term Project/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+
+    public Canvas quitMenu;
+    public Button startText;
+    public Button exitText;
+
+	// Use this for initialization
+	void Start () {
+
+        quitMenu = quitMenu.GetComponent<Canvas>();
+        startText = startText.GetComponent<Button>();//start button
+        exitText = exitText.GetComponent<Button>();//exit button
+        quitMenu.enabled = false;//keeps menu from being scene during the game playing process
+    }
+	
+	public void ExitPress()
+    {
+        quitMenu.enabled = true;//whenever you hit the exit button you enable the menu to be displayed
+        startText.enabled = false;
+        exitText.enabled = false;
+    }
+
+    public void NoPress()
+    {
+        quitMenu.enabled = false;
+        startText.enabled = true;
+        exitText.enabled = true;
+    }
+
+    public void StartLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Term Project/Assets/Scripts/PlayAgain.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayAgain : MonoBehaviour {
+   
+        public Button start_button;
+        private new string tag;
+
+        // Use this for initialization
+        void Start()
+        {
+            Button btn = start_button.GetComponent<Button>();
+            tag = btn.tag;
+            btn.onClick.AddListener(StartOnClick);
+
+        }
+
+        void StartOnClick()
+        {
+            if (tag == "Original")
+            {
+                Debug.Log(tag);
+                SceneManager.LoadScene("Original-Game", LoadSceneMode.Single);
+            }
+           
+        }
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    
+}
+
